f68f1a07600f3f79bdd41177805fcf063b55d697
Kenneth Shaw <lucky7chess@gmail.com>
2025-01-11 12:43:39 -0500

    trying again

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,41 +3,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/spring25/__docusaurus/debug',
-    component: ComponentCreator('/spring25/__docusaurus/debug', 'd6c'),
-    exact: true
-  },
-  {
-    path: '/spring25/__docusaurus/debug/config',
-    component: ComponentCreator('/spring25/__docusaurus/debug/config', 'd51'),
-    exact: true
-  },
-  {
-    path: '/spring25/__docusaurus/debug/content',
-    component: ComponentCreator('/spring25/__docusaurus/debug/content', '1d0'),
-    exact: true
-  },
-  {
-    path: '/spring25/__docusaurus/debug/globalData',
-    component: ComponentCreator('/spring25/__docusaurus/debug/globalData', 'b80'),
-    exact: true
-  },
-  {
-    path: '/spring25/__docusaurus/debug/metadata',
-    component: ComponentCreator('/spring25/__docusaurus/debug/metadata', '1f1'),
-    exact: true
-  },
-  {
-    path: '/spring25/__docusaurus/debug/registry',
-    component: ComponentCreator('/spring25/__docusaurus/debug/registry', 'b22'),
-    exact: true
-  },
-  {
-    path: '/spring25/__docusaurus/debug/routes',
-    component: ComponentCreator('/spring25/__docusaurus/debug/routes', '1e6'),
-    exact: true
-  },
-  {
     path: '/spring25/lectures',
     component: ComponentCreator('/spring25/lectures', '31c'),
     exact: true
